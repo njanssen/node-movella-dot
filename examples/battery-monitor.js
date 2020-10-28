c4e076747f000c7248361a47054cc918d62040df
Niels Janssen <niels@vliegwerk.com>
2020-10-28 10:51:57 +0100

    Fixed debug label for example

--- examples/battery-monitor.js
+++ examples/battery-monitor.js
@@ -1,6 +1,6 @@
 import createDebug from 'debug'
 import xsensManager from '../index.js'
-const debug = createDebug('xsens-dot:example')
+const debug = createDebug('xsens:example')
 
 xsensManager.on('error', (error) => {
     debug(error)
