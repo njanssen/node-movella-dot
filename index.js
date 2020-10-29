1be96c5ea55e9eda174487f28a64100f1a80c7d5
Niels Janssen <niels@vliegwerk.com>
2020-10-29 23:11:48 +0100

    Exporting renamed XSENS_DOT_PAYLOAD_TYPE constant

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import XsensManager from './XsensManager.js'
-import { XSENS_DOT_PAYLOAD } from './constants.js'
+import { XSENS_DOT_PAYLOAD_TYPE } from './constants.js'
 
-export const PAYLOAD_TYPE = XSENS_DOT_PAYLOAD
+export const PAYLOAD_TYPE = XSENS_DOT_PAYLOAD_TYPE
 export default new XsensManager().getInstance()
